f7d2ef01142d249ea49e7e79f465e5de4534a446
Сергеев Дмитрий Сергеевич <dima79346@gmail.com>
2024-08-13 13:57:21 +0300

    Добавлениe: TestDisplayer

--- TestingSystem/Model/Test.cs
+++ TestingSystem/Model/Test.cs
@@ -10,6 +10,25 @@ using System.Threading.Tasks;
 
 namespace TestingSystem.Model
 {
+    [Table("test_displayer")]
+    public sealed partial class TestDisplayer : ObservableObject
+    {
+        [PrimaryKey, AutoIncrement]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        private ObservableCollection<Test> _tests = [];
+        [Column("_tests")]
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public ObservableCollection<Test> Tests
+        {
+            get => _tests;
+            set => SetProperty(ref _tests, value);
+        }
+
+        public ObservableCollection<string> Category { get; set; }
+
+    }
 
 
     [Table("test")]
@@ -18,14 +37,21 @@ namespace TestingSystem.Model
         [PrimaryKey, AutoIncrement]
         [Column("Id")]
         public int Id { get; set; }
-        public string category;
-        private ObservableCollection<QuestionTest> _questionTests = [];
+        [ObservableProperty]
+        public string _category;
+
+        [Column("test_displayer_id")]
+        [ForeignKey(typeof(Test))]
+        public int TestDisplayerId { get; set; }
 
         public Test(string category)
         {
-            this.category = category;
+            Category = category;
         }
 
+       
+
+        private ObservableCollection<QuestionTest> _questionTests = [];
         [Column("question_tests")]
         [OneToMany(CascadeOperations = CascadeOperation.All)]
         public ObservableCollection<QuestionTest> QuestionTests
