b3cf07e2acd3efd1e9cdc97a80334570dff1383f
Сергеев Дмитрий Сергеевич <dima79346@gmail.com>
2024-08-13 12:55:05 +0300

    Добавьте файлы проекта.

---
+++ TestingSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestingSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
