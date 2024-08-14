a1da54cfd08dfd1bb7115d56c0e42710e14a6a9b
Сергеев Дмитрий Сергеевич <dima79346@gmail.com>
2024-08-15 01:33:55 +0300

    Перенос ServicesExtensions в отдельный файл

--- TestingSystem/MauiProgram.cs
+++ TestingSystem/MauiProgram.cs
@@ -11,32 +11,6 @@ using TestingSystem.ViewModel;
 
 namespace TestingSystem
 {
-    public static class ServicesExtensions
-    {
-        public static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder)
-        {
-            builder.Services.AddTransient<AddQuestionTestViewModel>().AddTransient<AddQuestionTestPage>();
-            builder.Services.AddTransient<PassingTestViewModel>().AddTransient<PassingTestPage>();
-            //builder.Services.AddTransient<EditorEventViewModel>().AddTransient<EditorEventPage>();
-            //builder.Services.AddTransient<StatisticsViewModel>().AddTransient<StatisticsPage>();
-            //builder.Services.AddTransient<MailingViewModel>().AddTransient<MailingPage>();
-            //builder.Services.AddSingleton<IMailAccount, MailAccount>();
-            builder.Services.AddSingleton<IPopupService, CommunityToolkit.Maui.PopupService>()
-                .AddTransientPopup<SavingTestPopup, SavingTestViewModel>();
-
-           
-            //builder.Services.AddTransient<AddScheduledEventViewModel>().AddTransient<AddScheduledEventPage>();
-            //builder.Services.AddTransient<GuestVerificationTableViewModel>().AddTransient<GuestVerificationTablePage>();
-            //builder.Services.AddTransient<SettingsPage>().AddTransient<SettingsViewModel>();
-            //builder.Services.AddSingleton<IDataService, DataService>();
-            builder.Services.AddSingleton<ILocalDbService, LocalDbService>();
-            builder.Services.AddSingleton<INavigationService, NavigationService>();
-            //builder.Services.AddTransient<ListOfEventsPage>().AddTransient<ListOfEventsViewModel>();
-            builder.Services.AddSingleton<MainViewModel>();
-            builder.Services.AddSingleton<MainPage>();
-            return builder;
-        }
-    }
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
---
+++ TestingSystem/ServicesExtensions.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using TestingSystem.Navigation;
+using TestingSystem.Service;
+using TestingSystem.Service.Interface;
+using TestingSystem.View;
+
+using TestingSystem.View.ViewPopup;
+using TestingSystem.ViewModel;
+
+namespace TestingSystem
+{
+    public static class ServicesExtensions
+    {
+        public static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder)
+        {
+            builder.Services.AddTransient<AddQuestionTestViewModel>().AddTransient<AddQuestionTestPage>();
+            builder.Services.AddTransient<PassingTestViewModel>().AddTransient<PassingTestPage>();
+            builder.Services.AddSingleton<IPopupService, CommunityToolkit.Maui.PopupService>()
+                .AddTransientPopup<SavingTestPopup, SavingTestViewModel>();
+            builder.Services.AddSingleton<ILocalDbService, LocalDbService>();
+            builder.Services.AddSingleton<INavigationService, NavigationService>();
+            builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton<MainPage>();
+            return builder;
+        }
+    }
+}
